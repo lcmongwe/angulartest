cb2217ba02cc850e3d6863cd58ef730adda12904
lcmongwe <lcmongwe@gmail.com>
2022-04-06 21:26:35 +0300

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'doctors appointment',
+    day: 'may 5th at 2:30pm',
+    reminder: true,
+  },
+
+  {
+    id: 1,
+    text: 'go to church',
+    day: 'jan 12th at 7:30 am',
+    reminder: true,
+  },
+
+  {
+    id: 3,
+    text: 'go the market',
+    day: 'june 10th at 12:30pm',
+    reminder: true,
+  },
+];
